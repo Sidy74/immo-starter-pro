686746234c1907d2aacaf193af20dd97873aee99
Sidy74 <snagatod74@gmail.com>
2026-05-10 14:45:18 +0000

    feat: add  theme variables

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,22 @@
 import { Component, signal } from '@angular/core';
+import { LucideMoon, LucideSun, provideLucideIcons, LucideComponent } from '@lucide/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [LucideSun, LucideMoon],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers: [
+
+  ]
+
 })
 export class App {
   protected readonly title = signal('immo-starter-pro');
+
+  sun = LucideSun;
+
+  toggleDarkMode() {
+    document.documentElement.classList.toggle('dark');
+  }
 }
