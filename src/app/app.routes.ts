2614ae961ae3d8924dfbf680442905e6ea515400
Sidy74 <snagatod74@gmail.com>
2026-05-13 08:52:23 +0000

    feat(auth): implement modern split-screen login and register with signals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { PropertyList } from './shared/components/property-list/property-list.component';
+import { PropertyList } from './components/property-list/property-list.component';
 import { PropertyDetailsComponent } from './components/property-details.component/property-details.component';
+import { AuthComponent } from './components/login/auth.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'properties', pathMatch: 'full' },
   { path: 'properties', component: PropertyList },
   { path: 'property/:id', component: PropertyDetailsComponent },
+  { path: 'auth', component: AuthComponent },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { NavigationComponent } from './components/navigation.component/navigation.component';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +12,11 @@ export class App {
   protected readonly title = signal('immo-starter-pro');
   toggleDarkMode() {
     document.documentElement.classList.toggle('dark');
+  }
+  private router = inject(Router);
+
+  // Signal ou fonction pour vérifier si on est sur la page auth
+  isAuthPage(): boolean {
+    return this.router.url === '/auth';
   }
 }
